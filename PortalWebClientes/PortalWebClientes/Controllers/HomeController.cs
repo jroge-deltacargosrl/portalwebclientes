1aa66d7142561c9e991f2de51f86865a43e8dded
Jroge <jorgerodrigotorrez@gmail.com>
2019-07-18 15:03:31 -0400

    Portal web estructurado

--- PortalWebClientes/PortalWebClientes/Controllers/HomeController.cs
+++ PortalWebClientes/PortalWebClientes/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace PortalWebClientes.Controllers
@@ -12,19 +9,29 @@ namespace PortalWebClientes.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult About(string email)
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
+            ViewBag.email = email;
+            List<User> listaDeUsuarios = new List<User>()
+            {
+                new User{email=email,name="Jorge"},
+                new User{email="OtroEmail",name="Jorge2"}
+            };
+            return View(listaDeUsuarios);
         }
-
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
+    }
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+
     }
 }
