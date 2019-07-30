7900d5cb47d9dd819c774fb5041ed36347f69ccc
Jroge <jorgerodrigotorrez@gmail.com>
2019-07-30 16:47:03 -0400

    Login con Metronic implementado

--- PortalWebClientes/Controllers/HomeController.cs
+++ PortalWebClientes/Controllers/HomeController.cs
@@ -5,33 +5,17 @@ namespace PortalWebClientes.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public ActionResult Index()
         {
+            //VERIFICACION DE CREDENCIALES
+            ViewBag.nombrePersona = "Jorge";
             return View();
         }
-        [HttpPost]
-        public ActionResult About(string email)
-        {
-            ViewBag.Message = "Your application description page.";
-            ViewBag.email = email;
-            List<User> listaDeUsuarios = new List<User>()
-            {
-                new User{email=email,name="Jorge"},
-                new User{email="OtroEmail",name="Jorge2"}
-            };
-            return View(listaDeUsuarios);
-        }
-        [HttpGet]
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-            return View();
-        }
-    }
-    public class User
-    {
-        public string name { get; set; }
-        public string email { get; set; }
-
     }
 }
