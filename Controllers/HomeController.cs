8c4c0c749edffcc6e8ed608ec230182069e80882
Jroge <jorgerodrigotorrez@gmail.com>
2019-08-19 08:02:51 -0400

    Timeline

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,16 +30,16 @@ namespace PortalWebCliente.Controllers
                 {
                     var client = new RestClient("http://deltawebapi.azurewebsites.net/api/v1/operations/");
                     var request = new RestRequest(Method.GET);
-                    request.AddHeader("cache-control", "no-cache");
-                    request.AddHeader("Connection", "keep-alive");
-                    request.AddHeader("Content-Length", "26");
-                    request.AddHeader("Accept-Encoding", "gzip, deflate");
-                    request.AddHeader("Cookie", "ARRAffinity=419a3fd042e09f6a2a8c01997b56c1bc63e7fa41cfbb8e4472612963a6dd2564");
-                    request.AddHeader("Host", "deltawebapi.azurewebsites.net");
-                    request.AddHeader("Postman-Token", "2ee76cf0-4044-4abf-a14e-653ce68188b3,627cc292-00b1-4912-be84-9981691d9cf5");
-                    request.AddHeader("Cache-Control", "no-cache");
-                    request.AddHeader("Accept", "*/*");
-                    request.AddHeader("User-Agent", "PostmanRuntime/7.15.2");
+                    //request.AddHeader("cache-control", "no-cache");
+                    //request.AddHeader("Connection", "keep-alive");
+                    //request.AddHeader("Content-Length", "26");
+                    //request.AddHeader("Accept-Encoding", "gzip, deflate");
+                    //request.AddHeader("Cookie", "ARRAffinity=419a3fd042e09f6a2a8c01997b56c1bc63e7fa41cfbb8e4472612963a6dd2564");
+                    //request.AddHeader("Host", "deltawebapi.azurewebsites.net");
+                    //request.AddHeader("Postman-Token", "c519f40d-72c6-4ef4-9543-9775dbfab5dd,a3a83752-3a36-4e76-8d97-018a5a21708d");
+                    //request.AddHeader("Cache-Control", "no-cache");
+                    //request.AddHeader("Accept", "*/*");
+                    //request.AddHeader("User-Agent", "PostmanRuntime/7.15.2");
                     request.AddHeader("Content-Type", "application/json");
                     request.AddParameter("undefined", "{\n\t\"id\":7,\n\t\"message\":\"\"\n}", ParameterType.RequestBody);
                     IRestResponse response = client.Execute(request);
@@ -51,7 +51,7 @@ namespace PortalWebCliente.Controllers
                         //Pedir el nombre de la persona
                         //Convertir el json que me llega a una List
                         //List = json.toList();
-                        persona.userName = json.Length.ToString();
+                        ViewBag.lista = json;
                         return View(persona);
                     }
                     else if (respuesta == 2)
