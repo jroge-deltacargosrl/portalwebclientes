0dfdcf2dc8cc90a12c129c6c168794d5716d5c82
Jroge <jorgerodrigotorrez@gmail.com>
2019-10-17 12:03:02 -0400

    Ventana emergente

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,6 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using PortalWebCliente.Models;
-using RestSharp;
-using PortalWebCliente.Models.Infrastructure;
-using Newtonsoft.Json;
 using static PortalWebCliente.Utils.SessionExtensions;
 
 namespace PortalWebCliente.Controllers
@@ -19,113 +16,6 @@ namespace PortalWebCliente.Controllers
             ViewBag.estadoContraseña = estadoContraseña;
             ViewBag.antiguoUsuario = antiguoUsuario;
             return View();
-        }
-       
-        [HttpGet("/Home/{projectName}", Name = "aux")]
-        public IActionResult TimeLineOperacion(string projectName)
-        {
-            List<ProjectModel> listaDeProyectos =
-                HttpContext.Session.getObjectFromJson
-                <List<ProjectModel>>("listaDeProyectosJSON");
-            foreach (ProjectModel proyecto in listaDeProyectos)
-            {
-                if (proyecto.name.Equals(projectName))
-                {
-                    UserResponse usuarioActual = HttpContext.Session.getObjectFromJson<UserResponse>("usuarioResponseJSON");
-                    ViewBag.userEmail = usuarioActual.email;
-                    return View(proyecto);
-                }
-            }
-            //NUNCA PASA POR AQUI
-            ProjectModel operacion = new ProjectModel();
-            operacion.stages = new List<StageModel>()
-            {
-                new StageModel()
-                {
-                    id=1,
-                    name="Etapa1",
-                    projectId=1,
-                    sequence=1,
-                    tasks=new List<TaskModel>()
-                    {
-                        new TaskModel
-                        {
-                            id=1,
-                            name="Tarea1",
-                            projectId=1,
-                            stageId=1,
-                            date_start=new DateTime(2019,01,01),
-                            kanbanState="normal"
-                        },
-                        new TaskModel
-                        {
-                            id=2,
-                            name="Tarea2",
-                            projectId=1,
-                            stageId=1,
-                            date_start=new DateTime(2019,01,02),
-                            kanbanState="done"
-                        }
-                    }
-                },
-                new StageModel()
-                {
-                    id=2,
-                    name="etapa2",
-                    projectId=2,
-                    sequence=2,
-                    tasks=new List<TaskModel>()
-                    {
-                        new TaskModel
-                        {
-                            id=3,
-                            name="Tarea3",
-                            projectId=2,
-                            stageId=2,
-                            date_start=new DateTime(2019,01,01),
-                            kanbanState="normal"
-                        },
-                        new TaskModel
-                        {
-                            id=4,
-                            name="Tarea4",
-                            projectId=2,
-                            stageId=2,
-                            date_start=new DateTime(2019,01,02),
-                            kanbanState="done"
-                        }
-                    }
-                },
-                new StageModel()
-                {
-                    id=3,
-                    name="etapa3",
-                    projectId=3,
-                    sequence=3,
-                    tasks=new List<TaskModel>()
-                    {
-                        new TaskModel
-                        {
-                            id=5,
-                            name="Tarea5",
-                            projectId=3,
-                            stageId=3,
-                            date_start=new DateTime(2019,01,01),
-                            kanbanState="normal"
-                        },
-                        new TaskModel
-                        {
-                            id=6,
-                            name="Tarea6",
-                            projectId=3,
-                            stageId=3,
-                            date_start=new DateTime(2019,01,02),
-                            kanbanState="done"
-                        }
-                    }
-                }
-            };
-            return View(operacion);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Controllers/OperationsController.cs
+++ Controllers/OperationsController.cs
@@ -1,16 +1,159 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PortalWebCliente.Models;
 using PortalWebCliente.Models.Infrastructure;
 using PortalWebCliente.Utils;
 using RestSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 
 namespace PortalWebCliente.Controllers
 {
     public class OperationsController : Controller
     {
         private string urlRequest = @"http://deltacargoapi.azurewebsites.net/api/v1/";
+        private IHostingEnvironment _hostingEnv;
+
+        public OperationsController(IHostingEnvironment hostingEnv)
+        {
+            _hostingEnv = hostingEnv;
+        }
+
+        [HttpGet("/Timeline/{projectName}", Name = "aux")]
+        public IActionResult Timeline(string projectName)
+        {
+            List<ProjectModel> listaDeProyectos =
+                HttpContext.Session.getObjectFromJson
+                <List<ProjectModel>>("listaDeProyectosJSON");
+            foreach (ProjectModel proyecto in listaDeProyectos)
+            {
+                if (proyecto.name.Equals(projectName))
+                {
+                    UserResponse usuarioActual = HttpContext.Session.getObjectFromJson<UserResponse>("usuarioResponseJSON");
+                    ViewBag.userEmail = usuarioActual.email;
+                    return View(proyecto);
+                }
+            }
+            //NUNCA PASA POR AQUI
+            ProjectModel operacion = new ProjectModel();
+            operacion.stages = new List<StageModel>()
+            {
+                new StageModel()
+                {
+                    id=1,
+                    name="Etapa1",
+                    projectId=1,
+                    sequence=1,
+                    tasks=new List<TaskModel>()
+                    {
+                        new TaskModel
+                        {
+                            id=1,
+                            name="Tarea1",
+                            projectId=1,
+                            stageId=1,
+                            date_start=new DateTime(2019,01,01),
+                            kanbanState="normal"
+                        },
+                        new TaskModel
+                        {
+                            id=2,
+                            name="Tarea2",
+                            projectId=1,
+                            stageId=1,
+                            date_start=new DateTime(2019,01,02),
+                            kanbanState="done"
+                        }
+                    }
+                },
+                new StageModel()
+                {
+                    id=2,
+                    name="etapa2",
+                    projectId=2,
+                    sequence=2,
+                    tasks=new List<TaskModel>()
+                    {
+                        new TaskModel
+                        {
+                            id=3,
+                            name="Tarea3",
+                            projectId=2,
+                            stageId=2,
+                            date_start=new DateTime(2019,01,01),
+                            kanbanState="normal"
+                        },
+                        new TaskModel
+                        {
+                            id=4,
+                            name="Tarea4",
+                            projectId=2,
+                            stageId=2,
+                            date_start=new DateTime(2019,01,02),
+                            kanbanState="done"
+                        }
+                    }
+                },
+                new StageModel()
+                {
+                    id=3,
+                    name="etapa3",
+                    projectId=3,
+                    sequence=3,
+                    tasks=new List<TaskModel>()
+                    {
+                        new TaskModel
+                        {
+                            id=5,
+                            name="Tarea5",
+                            projectId=3,
+                            stageId=3,
+                            date_start=new DateTime(2019,01,01),
+                            kanbanState="normal"
+                        },
+                        new TaskModel
+                        {
+                            id=6,
+                            name="Tarea6",
+                            projectId=3,
+                            stageId=3,
+                            date_start=new DateTime(2019,01,02),
+                            kanbanState="done"
+                        }
+                    }
+                }
+            };
+            return View(operacion);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            UserResponse actualUser = HttpContext.Session.getObjectFromJson<UserResponse>("usuarioResponseJSON");
+            List<ProjectModel> operations = responseFromOperationAPI(actualUser.id);
+            List<ProjectModel> runningOperations = new List<ProjectModel>();
+            List<ProjectModel> historyOperations = new List<ProjectModel>();
+            foreach (ProjectModel p in operations)
+            {
+                if (!isFinalizedOperation(p))
+                {
+                    runningOperations.Add(p);
+                }
+                else
+                {
+                    historyOperations.Add(p);
+                }
+            }
+            HttpContext.Session.setObjectAsJson("listaDeProyectosJSON", operations);
+            ViewBag.userEmail = actualUser.email;
+            ViewBag.runningOperations = runningOperations;
+            ViewBag.historyOperations = historyOperations;
+            return View();
+        }
 
         [HttpPost]
         public IActionResult Index(UserModel usuario)
@@ -84,29 +227,22 @@ namespace PortalWebCliente.Controllers
             });
         }
 
-        [HttpGet]
-        public IActionResult Index()
+        [HttpPost]
+        public JsonResult UploadFile(ProjectModel project)
         {
-            UserResponse actualUser = HttpContext.Session.getObjectFromJson<UserResponse>("usuarioResponseJSON");
-            List<ProjectModel> operations = responseFromOperationAPI(actualUser.id);
-            List<ProjectModel> runningOperations = new List<ProjectModel>();
-            List<ProjectModel> historyOperations = new List<ProjectModel>();
-            foreach (ProjectModel p in operations)
+            IFormFile imageFile = project.stages[0].tasks[0].document;
+            string cad = "ERROR";
+            if (ModelState.IsValid)
             {
-                if (!isFinalizedOperation(p))
-                {
-                    runningOperations.Add(p);
-                }
-                else
-                {
-                    historyOperations.Add(p);
-                }
+                var filename = ContentDispositionHeaderValue.Parse(imageFile.ContentDisposition).FileName.Trim('"');
+                var targetDirectory = Path.Combine(_hostingEnv.WebRootPath, string.Format("Common\\Images\\"));
+                var savePath = Path.Combine(targetDirectory, filename);
+                imageFile.CopyTo(new FileStream(savePath, FileMode.Create));
+                //model.ImageFile = filename;
+                //_imageUploadAppService.UserCreate(model);
+                cad = "OK";
             }
-            HttpContext.Session.setObjectAsJson("listaDeProyectosJSON", operations);
-            ViewBag.userEmail = actualUser.email;
-            ViewBag.runningOperations = runningOperations;
-            ViewBag.historyOperations = historyOperations;
-            return View();
+            return Json(cad);
         }
 
         private bool isEmpty(string word)
